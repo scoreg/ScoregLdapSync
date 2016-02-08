de3772a37991c552ddece6e503be8481a21d864b
Vinzenz Stadtmueller <info@edgelab.de>
2016-02-08 14:26:23 +0100

    First Working

--- main.py
+++ main.py
@@ -2,16 +2,13 @@
 from services import memberService
 from ldap import ldapService, ldaputil
 from config import config
-from operator import itemgetter, attrgetter
-from passlib.hash import ldap_sha1 as lls
+from operator import attrgetter
 import logging
 
 logging.basicConfig(filename='ldap.log', level=logging.INFO)
 conf = config.read_conf('scoregLdapSync.ini')
 ldapService.connect(conf['ldap'])
 memberService.init(conf['Scoreg'])
-
-print(lls.encrypt("password"))
 
 members = memberService.get_all_scouts(conf['Scoreg']['OrgId'])
 
